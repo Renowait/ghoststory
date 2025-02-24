2e8b75b3e98529ae294787ee765ab42e63543c5a
Renowait <3651051541147@rmutr.ac.th>
2025-02-24 23:30:51 +0700

    ทำให่สามารถสรา้งแอพลงมือถือได้

---
+++ android/app/src/main/java/com/example/fluttepro/MainApplication.kt
@@ -0,0 +1,9 @@
+package com.example.fluttepro
+
+import io.flutter.app.FlutterApplication
+
+class MainApplication : FlutterApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- android/app/src/main/kotlin/com/example/fluttepro/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fluttepro/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.fluttepro
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
